ebce261958802d3ac80b4cbf79af69fe3f2d4c1d
jhonathanwesley <jhonwfdalves@gmail.com>
2025-06-20 18:42:12 -0300

    fix: Corrige erro de digitação na meta tag viewport do endpoint HTML feat: Adiciona arquivo de configuração para cSpell com palavras personalizadas

--- fastapp/app.py
+++ fastapp/app.py
@@ -27,7 +27,7 @@ def hello_html():
     <html lang="en">
     <head>
         <meta charset="UTF-8">
-        <meta name="viewport" content="widht=device-widht, initial-scale=1.0">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Hello World API endpoint</title>
     </head>
     <body>
